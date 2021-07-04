0f3a6bd928ee9620980de4a9f35f8abae421824c
Pete D <peted70@hotmail.com>
2021-07-04 14:55:18 +0100

    added other folders

---
+++ Source/WorldAnchorSample/WorldAnchorSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorldAnchorSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDANCHORSAMPLE_API AWorldAnchorSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
